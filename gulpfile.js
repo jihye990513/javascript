ab8092715c964535774ba849a4b898384b937241
choijihye <jhjh990513@naver.com>
2023-12-18 21:35:09 +0900

    세팅 최종 푸시

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
 'use strict';
 
 var gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+// var fs = require('fs');
+var path = require('path');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var spritesmith = require('gulp.spritesmith');
 
+var path = {
+  scss_src: 'src/scss/',
+  img_src: 'src/img/',
+};
 
-//Sass
-gulp.task('sass', function() {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/css'));
+gulp.task('sass', function () {
+  gulp.src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      indentType: 'tab',
+      indentWidth: 1
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sprite', async function () {
+  var spriteData = gulp.src(path.img_src + '/sp_img/*.png')
+    .pipe(spritesmith({
+      imgName: 'sp_img.png',
+      padding: 10,
+      cssName: '_sp_img.scss'
+    }));
+  spriteData.img.pipe(gulp.dest(path.img_src));
+  spriteData.css.pipe(gulp.dest(path.scss_src + 'sprite'));
 });
